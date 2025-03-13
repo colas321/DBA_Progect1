5a9291a46c40d557c2b4786883ce642300e53782
colas321 <98768870+colas321@users.noreply.github.com>
2025-03-12 21:08:46 -0400

    Update computer_store_sales.sql
    touch up

--- project_eer_diag/DATAFINALLY/computer_store_sales.sql
+++ project_eer_diag/DATAFINALLY/computer_store_sales.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
 -- Host: localhost    Database: computer_store
 -- ------------------------------------------------------
--- Server version	8.0.40
+-- Server version	8.0.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -39,7 +39,7 @@ CREATE TABLE `sales` (
   CONSTRAINT `part_id_fk` FOREIGN KEY (`part_id`) REFERENCES `parts` (`part_id`) ON UPDATE CASCADE,
   CONSTRAINT `prebuild_id_fk` FOREIGN KEY (`prebuilt_id`) REFERENCES `prebuilt` (`prebuilt_id`) ON UPDATE CASCADE,
   CONSTRAINT `warranty_id_fk` FOREIGN KEY (`warranty_id`) REFERENCES `warranties` (`warranty_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,6 +48,7 @@ CREATE TABLE `sales` (
 
 LOCK TABLES `sales` WRITE;
 /*!40000 ALTER TABLE `sales` DISABLE KEYS */;
+INSERT INTO `sales` VALUES (1,1350.75,5,12,2,3,'2024-08-15'),(2,7899.99,8,25,1,2,'2025-06-30'),(3,499.50,2,5,3,1,'2023-11-10'),(4,10250.00,10,17,2,4,'2026-04-20'),(5,275.99,9,1,3,1,'2022-09-05'),(6,14299.25,7,30,1,1,'2027-01-18'),(7,6235.80,10,14,2,3,'2025-12-03'),(8,850.60,4,8,1,2,'2024-03-22'),(9,11560.99,3,22,3,4,'2026-07-14'),(10,3500.75,6,10,2,1,'2023-10-09'),(11,99.99,10,3,1,1,'2022-06-18'),(12,7800.00,10,18,3,2,'2025-09-27'),(13,12040.49,1,28,2,3,'2026-02-11'),(14,690.99,10,6,1,1,'2023-05-30'),(15,14999.99,10,20,3,4,'2027-08-01');
 /*!40000 ALTER TABLE `sales` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -60,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-12 20:35:42
+-- Dump completed on 2025-03-12 21:05:26
